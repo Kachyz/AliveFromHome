c2eb439d6a6ecb5e09fae57fd5ea5ec123c29001
CarlosEstrada <kachiiz@gmail.com>
2020-05-18 23:21:08 -0500

    Ejercicio 5, creacion de nodos

---
+++ 05_JS_ToDoList/funciones.js
@@ -0,0 +1,10 @@
+function agregar() {
+  let lista = document.getElementById("pendientes")
+  let elemento = document.getElementById("tarea").value
+
+  let nuevaEtiqueta = document.createElement("li");
+  let texto = document.createTextNode(elemento)
+  nuevaEtiqueta.appendChild(texto)
+  
+  lista.appendChild(nuevaEtiqueta)
+}
